5c1f9fb7bbd965e6b08fe13bbf55577db6ad6eee
Hurng-Chun Lee <h.lee@donders.ru.nl>
2016-11-24 11:22:43 +0100

    remove useless kenel setting in the Redis entrypoint script

--- docker/redis/start_redis.sh
+++ docker/redis/start_redis.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-# TODO: these three performance setting doesn't not take place in this way!! To be fixed.
-sysctl -w net.core.somaxconn=65535
-sysctl vm.overcommit_memory=1
-echo never > /sys/kernel/mm/transparent_hugepage/enabled
-
 # start the redis-server
 redis-server --protected-mode no --appendonly yes
