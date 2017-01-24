f4562dd50c757400c7395f138673b2f3002c2a95
Hurng-Chun Lee <h.lee@donders.ru.nl>
2017-01-24 20:51:55 +0100

    escape the '.' character in the awk delimitor specification

--- docker/streamer/bin/find-update-ds.sh
+++ docker/streamer/bin/find-update-ds.sh
@@ -3,4 +3,4 @@
 raw_dir=$1
 min_back=$2
 
-find $raw_dir -type f -path '*.ds/*' -mmin -${min_back} | awk -F '.ds' '{print $1".ds"}' | sort | uniq
+find $raw_dir -type f -path '*.ds/*' -mmin -${min_back} | awk -F '\\.ds' '{print $1".ds"}' | sort | uniq
