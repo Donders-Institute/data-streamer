b45fd2ef5fc4359c7dae4ea9db1ad87cf84807c7
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2019-08-20 16:49:46 +0200

    Add headers

--- docker/streamer-ui/src/components/Uploader/Uploader.tsx
+++ docker/streamer-ui/src/components/Uploader/Uploader.tsx
@@ -263,6 +263,9 @@ class UploaderApp extends React.Component<IProps & FormComponentProps, UploaderA
       name: 'file',
       multiple: true,
       action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+      headers: {
+        authorization: 'authorization-text',
+      },
       onChange: this.handleChange,
       showUploadList: false,
     };
