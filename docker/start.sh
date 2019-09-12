518cd233c1b2c3e7a33cc5b8d12c7af2f25a5a09
Hurng-Chun Lee <h.lee@donders.ru.nl>
2019-09-12 16:01:04 +0200

    update test container startup script

--- docker/start.sh
+++ docker/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo "starting streamer db and service ..."
-    docker-compose -f docker-compose.yml -f docker-compose.test.yml up service db
+if [ $# -gt 1 ]; then
+    echo "starting streamer $@ ..."
+    docker-compose -f docker-compose.yml -f docker-compose.test.yml up $@
 else
     echo "starting streamer db, service and ui ..."
     docker-compose -f docker-compose.yml -f docker-compose.test.yml up
