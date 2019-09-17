1782c0b94f03a290373a2aae7d2ed5fc66d1f1ff
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2019-09-17 08:45:46 +0200

    Add spinner

--- streamer-ui/client/src/components/Auth/Auth.tsx
+++ streamer-ui/client/src/components/Auth/Auth.tsx
@@ -1,7 +1,10 @@
+// consider axios
+
 const Auth = {
     isAuthenticated: false,
     authenticate(username: string, password: string) {
         this.isAuthenticated = true;
+        return "success";
     },
     signout() {
         this.isAuthenticated = false;
