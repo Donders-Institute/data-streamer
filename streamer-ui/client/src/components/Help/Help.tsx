a413e168ebe259927427b627b8d662dcbec522cb
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2019-09-27 15:40:48 +0200

    Update screenshots; update help

--- streamer-ui/client/src/components/Help/Help.tsx
+++ streamer-ui/client/src/components/Help/Help.tsx
@@ -19,7 +19,7 @@ const Help: React.FC = () => {
                 >
                     <h1>Help</h1>
                     <div>
-                        Please find instructions how to use the streamer UI below.
+                        Please find instructions below how to use the streamer UI.
                     </div>
                     <div style={{ zoom: 1, padding: "7px", border: "1px solid", background: "#f8f9fa", font: "95%", width: "25%", marginTop: 20 }}>
                         <div>
@@ -32,13 +32,24 @@ const Help: React.FC = () => {
                                 </a>
                                 <ol>
                                     <li>
-                                        Select file(s)
+                                        <a href="#quickguidelogin">
+                                            Login
+                                        </a>
                                     </li>
                                     <li>
-                                        Select project, subject, session, and data type
+                                        <a href="#quickguideselectfiles">
+                                            Select file(s)
+                                        </a>
                                     </li>
                                     <li>
-                                        Upload file(s)
+                                        <a href="#quickguideselectstructure">
+                                            Select project, subject, session, and data type
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#quickguideuploadfiles">
+                                            Upload file(s)
+                                        </a>
                                     </li>
                                 </ol>
                             </li>
@@ -50,26 +61,49 @@ const Help: React.FC = () => {
                         </ul>
                     </div>
 
-                    <h2 style={{ marginTop: 40 }}><a id="quickguide"></a>Quick guide</h2>
+                    <h2 id="quickguide" style={{ marginTop: 40 }}>Quick guide</h2>
 
-                    <h3>1. Select file(s)</h3>
-                    <div>
-                        <img src={process.env.PUBLIC_URL + "/images/screenshot-quick-guide-01.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
+                    <h3 id="quickguidelogin">1. Login</h3>
+                    <div style={{ marginTop: 20 }}>
+                        Login with your DCCN credentials.
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <img src={process.env.PUBLIC_URL + "/images/quick-guide-screenshot-00-login.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
                     </div>
 
-                    <h3 style={{ marginTop: 20 }}>2. Select project, subject, session, and data type</h3>
-                    <div>
-                        <img src={process.env.PUBLIC_URL + "/images/screenshot-quick-guide-01.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
+                    <h3 id="quickguideselectfiles" style={{ marginTop: 20 }}>2. Select file(s)</h3>
+                    <div style={{ marginTop: 20 }}>
+                        Select one or more files.
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <img src={process.env.PUBLIC_URL + "/images/quick-guide-screenshot-01-uploader-start.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <img src={process.env.PUBLIC_URL + "/images/quick-guide-screenshot-02-uploader-select-files.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
                     </div>
 
-                    <h3 style={{ marginTop: 20 }}>3. Upload file(s)</h3>
-                    <div>
-                        <img src={process.env.PUBLIC_URL + "/images/screenshot-quick-guide-01.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
+                    <h3 id="quickguideselectstructure" style={{ marginTop: 20 }}>3. Select project number, subject, session, and data type</h3>
+                    <div style={{ marginTop: 20 }}>
+                        Select project number, subject, session, and data type.
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <img src={process.env.PUBLIC_URL + "/images/quick-guide-screenshot-03-uploader-select-structure.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
                     </div>
 
-                    <h2 style={{ marginTop: 40 }}><a id="troubleshooting"></a>Troubleshooting</h2>
-                    <div>
-                        <Link to="/contact">Contact</Link> one of the adminstrators
+                    <h3 id="quickguideuploadfiles" style={{ marginTop: 20 }}>4. Upload file(s)</h3>
+                    <div style={{ marginTop: 20 }}>
+                        Press the upload button.
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        <img src={process.env.PUBLIC_URL + "/images/quick-guide-screenshot-04-uploader-progress.png"} style={{ width: "50%", border: "1px solid", boxShadow: "8px 8px 8px #ddd" }} />
+                    </div>
+                    <div style={{ marginTop: 20 }}>
+                        Upload another batch of files or log out when you are done.
+                    </div>
+
+                    <h2 id="troubleshooting" style={{ marginTop: 40 }}>Troubleshooting</h2>
+                    <div style={{ marginTop: 20 }}>
+                        Please <Link to="/contact">contact</Link> one of the adminstrators when encountering any issues.
                     </div>
 
                     <div>
