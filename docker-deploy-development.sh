cd705b1a4fa30759918b58a03c7dc7318d4ec007
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2019-12-10 16:45:04 +0100

    Update docker-deploy-development.sh

--- docker-deploy-development.sh
+++ docker-deploy-development.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# export variables defined in env-development.sh
+# export variables defined in env.sh
 set -a && source env.sh && set +a
 docker stack up -c docker-compose.yml -c docker-compose.swarm.yml --prune --with-registry-auth --resolve-image always streamer4user
