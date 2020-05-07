0219ae43d32ab5a461af86ad7f1dd9510e6fe26f
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2020-05-07 13:58:02 +0200

    Make modals larger

--- streamer-ui/packages/client/src/components/Uploader/utils.ts
+++ streamer-ui/packages/client/src/components/Uploader/utils.ts
@@ -15,16 +15,19 @@ export const formatBytes = (bytesAsString: string, decimals = 2) => {
     return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
 };
 
+// Allow as many lowercase and uppercase characters and numbers as needed.
 const regexpSubjectLabel = new RegExp("^[a-zA-Z0-9]+$");
 export const validateSubjectLabelInput = (text: string) => {
     return regexpSubjectLabel.test(text);
 };
 
+// Allow as many lowercase and uppercase characters and numbers as needed.
 const regexpSessionLabel = new RegExp("^[a-zA-Z0-9]+$");
 export const validateSessionLabelInput = (text: string) => {
     return regexpSessionLabel.test(text);
 };
 
+// Start with lowercase characters. Next, allow as many lowercase characters, numbers dashes, underscores, and ampersands as needed.
 const regexpSelectedDataTypeOtherInput = new RegExp("^[a-z][-a-z0-9_\&]+$");
 export const validateSelectedDataTypeOtherInput = (text: string) => {
     return regexpSelectedDataTypeOtherInput.test(text);
