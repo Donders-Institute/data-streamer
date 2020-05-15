68156bfaf5b72f9ac9a4aa637b6c83f5c5a17d8d
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2020-05-15 17:30:48 +0200

    Cleanup server; do not set content-type for form data

--- streamer-ui/packages/server/routes/content.js
+++ streamer-ui/packages/server/routes/content.js
@@ -9,14 +9,4 @@ var _hasJson = function (req, res, next) {
     next();
 }
 
-// Middleware to verify Content-Type 'multipart/form-data'
-var _hasFormData = function (req, res, next) {
-    if (!req.is("multipart/form-data")) {
-        return next(createError(400, "Invalid Content-Type"));
-    }
-
-    next();
-}
-
 module.exports.hasJson = _hasJson;
-module.exports.hasFormData = _hasFormData;
