c834fe7616a095429d596a2a8911c1ab2cc0c563
Rutger van Deelen <r.vandeelen@donders.ru.nl>
2020-05-18 09:09:53 +0200

    Rename /clean to /purge

--- clean_streamer_ui_db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ -z "$1" ]; then
-    echo "No URL supplied"
-    exit 1
-fi
-
-echo "cleaning streamer ui db ..."
-set -a && source env.sh && set +a && curl -X POST -u $STREAMER_UI_DB_USER:$STREAMER_UI_DB_PASSWORD -i -H 'Accept:application/json' $1/clean
