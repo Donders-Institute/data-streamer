fd92a30214bc2733c741c39e476571dc72b65a6f
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2020-06-12 09:35:44 +0200

    Remove some logging

--- streamer-ui/packages/server/routes/upload.js
+++ streamer-ui/packages/server/routes/upload.js
@@ -99,8 +99,6 @@ var _begin = async function(req, res, next) {
     const sessionLabel = req.body.sessionLabel;
     const dataType = req.body.dataType;
 
-    console.log("test1");
-
     // Create the streamer UI buffer directory if it does not exist
     const dirname = utils.getStreamerUIBufferDirname(projectNumber, subjectLabel, sessionLabel, dataType);
     if (!dirname) {
