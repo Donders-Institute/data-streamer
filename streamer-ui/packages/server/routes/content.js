a515450dc5bfcd40fba6415c9f12250468bb0ad6
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2020-06-16 14:22:46 +0200

    Make hasJson check more relaxed

--- streamer-ui/packages/server/routes/content.js
+++ streamer-ui/packages/server/routes/content.js
@@ -2,7 +2,8 @@ const createError = require("http-errors");
 
 // Middleware to verify Content-Type 'application/json' in header
 var _hasJson = function(req, res, next) {
-    if (!req.is('application/json')) {
+    const contentType = req.get('Content-Type');
+    if (!contentType.startsWith('application/json')) {
         return next(createError(400, "Invalid Content-Type"));
     }
 
