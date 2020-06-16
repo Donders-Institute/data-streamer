9e065d232da2357aa5dd30de48cb44a0e8976263
Rutger van Deelen <R.vanDeelen@donders.ru.nl>
2020-06-16 09:07:19 +0200

    Change end point for purge script

--- streamer-ui/packages/server/purge-db.sh
+++ streamer-ui/packages/server/purge-db.sh
@@ -4,4 +4,4 @@ credentials=$STREAMER_UI_DB_USER:$STREAMER_UI_DB_PASSWORD
 url=http://$STREAMER_UI_HOST:$STREAMER_UI_PORT
 
 echo "Purge streamer ui db ..."
-curl -X POST -u $credentials -i -H 'Content-Type: application/json' $url
+curl -X POST -u $credentials -i -H 'Content-Type: application/json' $url/api/purge
