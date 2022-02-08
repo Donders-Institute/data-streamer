cfca1d9ccff1f99875b98cb375d6d58e7759714a
Hurng-Chun Lee <h.lee@donders.ru.nl>
2022-02-08 14:27:42 +0100

    display DAC location

--- streamer-ui/packages/client/src/scenes/Uploader/components/TargetPath/TargetPath.tsx
+++ streamer-ui/packages/client/src/scenes/Uploader/components/TargetPath/TargetPath.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Tooltip } from "antd";
 
-import { UploadState } from "../../../../types/types";
+import { StagerResult, UploadState } from "../../../../types/types";
+import { baseUrl, fetchOnce } from "../../../../services/fetch/fetch";
 
 interface TargetPathProps {
     uploadState: UploadState;
@@ -15,6 +16,40 @@ const TargetPath: React.FC<TargetPathProps> = ({ uploadState }) => {
     const sessionLabelInput = uploadState.structureSelection.sessionLabelInput;
     const dataTypeInput = uploadState.structureSelection.dataTypeInput;
     const dataTypeOtherInput = uploadState.structureSelection.dataTypeOtherInput;
+
+    const [projectNumber, setProjectNumber] = useState("");
+    const [dacIdentifier, setDacIdentifier] = useState("");
+
+    // call out to resolve the identifier of the associated DAC in the repository 
+    useEffect(() => {
+
+        const url = baseUrl() + "/stager/dac/" + projectNumber;
+        const headers = new Headers(
+            {
+                'Content-Type': 'application/json',
+            }
+        );
+
+        fetchOnce(
+            {
+                url: url,
+                options: {
+                    headers: headers,
+                },
+                timeout: 30*1000,  // 30 second timeout
+            }
+        ).then((response) => {
+            const collName = response.data && (response.data as StagerResult).collName;
+            if (collName) {
+                // converting collName to collectionIdentifier: /nl.ru.donders/di/dccn/DAC_3055000.01_123 --> di.dccn.DAC_3055000.01_123
+                setDacIdentifier(collName.split("/").slice(1).join("."));
+            }
+        }).catch((reason) => {
+            // log error silently, and reset the DAC identifier
+            console.warn("fail to get DAC for project ", projectNumber, " :", reason);
+            setDacIdentifier("");
+        });
+    }, [projectNumber]);
 
     let drivePath = (
         <span style={{ fontWeight: "bold", color: "#52c41a" }}>P:</span>
@@ -46,9 +81,11 @@ const TargetPath: React.FC<TargetPathProps> = ({ uploadState }) => {
     );
 
     if (projectNumberInput.isSelected) {
+        const p = projectNumberInput.value;
+        ( p !== projectNumber ) && setProjectNumber(p);
         projectNumberPath = (
             <span style={{ fontWeight: "bold", color: "#52c41a" }}>
-                {projectNumberInput.value}
+                {p}
             </span>
         );
     }
@@ -88,8 +125,8 @@ const TargetPath: React.FC<TargetPathProps> = ({ uploadState }) => {
     }
 
     return (
-        <React.Fragment>
-            <Tooltip placement="bottomLeft" title="Destination folder where files are uploaded to">
+        <>
+            <Tooltip placement="bottomLeft" title="Upload destination in the project storage">
                 {drivePath}
                 {backwardSlashPath}
                 {projectNumberPath}
@@ -105,7 +142,22 @@ const TargetPath: React.FC<TargetPathProps> = ({ uploadState }) => {
                 {dataTypePath}
                 {backwardSlashPath}
             </Tooltip>
-        </React.Fragment>
+            <br/>
+            <Tooltip placement="bottomLeft" title="Upload destination in the Donders Repository">
+                {dacIdentifier}
+                {backwardSlashPath}
+                {rawPath}
+                {backwardSlashPath}
+                {subjectPath}
+                {subjectLabelPath}
+                {backwardSlashPath}
+                {sessionPath}
+                {sessionLabelPath}
+                {backwardSlashPath}
+                {dataTypePath}
+                {backwardSlashPath}
+            </Tooltip>            
+        </>
     );
 };
 
